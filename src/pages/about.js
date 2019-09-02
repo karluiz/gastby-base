4341b63e87a3052578198ec9020db2ac3ff8d474
Carlos Gallardo Arenas <cgallardo@cleverit.cl>
2019-09-02 00:05:28 -0400

    so far

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+export default () => (
+  <>
+    <h1>About me!</h1>
+    <p>this page is about me ;D</p>
+    <Link to="/">&larr; back to home</Link>
+  </>
+);
